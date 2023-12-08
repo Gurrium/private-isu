7542731770e60d06a41f4563d7fa2d52f6e723bc
gurrium <gurrium@hatena.ne.jp>
2023-12-08 21:48:56 +0900

    mimeは必要だった

--- webapp/go/app.go
+++ webapp/go/app.go
@@ -502,7 +502,7 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 	results := []Post{}
 
 	query := `
-		SELECT posts.id, posts.body, posts.created_at,
+		SELECT posts.id, posts.body, posts.mime, posts.created_at,
 		users.account_name AS "users.account_name", users.authority AS "users.authority"
 		FROM posts
 		JOIN users ON posts.user_id = users.id
