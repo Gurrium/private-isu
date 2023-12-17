6555e846d41bccd7d7f17f7a8b576ee71d4e0b2f
gurrium <gurrium@hatena.ne.jp>
2023-12-17 21:58:51 +0900

    バッファのサイズを減らしてみる

--- webapp/go/app.go
+++ webapp/go/app.go
@@ -689,7 +689,7 @@ var templatePostByteArray = [...][]byte{
 
 func templatePost(w io.Writer, post Post) {
 	cacheKey := []byte(fmt.Sprintf("template_post_%d", post.ID))
-	buf := bytes.NewBuffer(make([]byte, 0, 850+1024))
+	buf := bytes.NewBuffer(make([]byte, 0, 850+512))
 
 	cached, err := cache.Get(cacheKey)
 	if err == nil {
