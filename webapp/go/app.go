d689e9c8fc76aa5d1be8a40c54814447969baa1c
gurrium <gurrium@hatena.ne.jp>
2023-12-20 23:08:34 +0900

    Revert "SetMaxOpenConns(5)"
    This reverts commit 06e185afdef14ebb74ca51d8f7c61fe4ca9161c5.

--- webapp/go/app.go
+++ webapp/go/app.go
@@ -1396,7 +1396,7 @@ func main() {
 	}
 	defer db.Close()
 
-	maxOpenConns := 5
+	maxOpenConns := 20
 	db.SetMaxOpenConns(maxOpenConns)
 	db.SetMaxIdleConns(maxOpenConns)
 	db.SetConnMaxLifetime(time.Second * time.Duration(maxOpenConns))
